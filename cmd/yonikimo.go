9f8eba2e3c4bdf8dceb02e07e7c8a7f47620d69a
raw34 <raw34@sina.com>
2020-08-09 19:05:27 +0800

    update

--- cmd/yonikimo.go
+++ cmd/yonikimo.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/antchfx/htmlquery"
 	"github.com/gocolly/colly/v2"
 	"log"
+	"os"
 	"strings"
 )
 
 func main() {
+	Init()
 	// Instantiate default collector
 	c := colly.NewCollector(
 		// Visit only domains
@@ -28,15 +31,27 @@ func main() {
 			title := htmlquery.FindOne(node, "td[2]/a/text()")
 			url := htmlquery.FindOne(node, "td[2]/a/@href")
 
-			log.Println(no, title, url)
+			log.Print(no, title, url)
 		}
 	})
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL.String())
+		log.Print("Visiting", r.URL.String())
 	})
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("http://yonikimo.com/story.html")
+}
+
+func Init() {
+	logFile, err := os.OpenFile("logs/cmd.log", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0644)
+
+	if err != nil {
+		fmt.Println("open log file failed, err:", err)
+		return
+	}
+
+	log.SetOutput(logFile)
+	log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
 }
